0dbd29fbe30f69d6f011947c9957978a50a4e480
Godslove Udo <udogodslove@gmail.com>
2021-06-22 10:51:13 -0700

    Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("Holberton\n");
+    return (0);
+}
