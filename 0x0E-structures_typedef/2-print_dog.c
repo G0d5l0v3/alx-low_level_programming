a09f9d5482fea33b515dc0bbd6c7aa0ff300eb2a
Godslove Udo <udogodslove@gmail.com>
2021-07-19 05:39:01 -0700

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,3 +12,4 @@ void print_dog(struct dog *d)
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->name != NULL ? d->owner : "(nil)");
+}
